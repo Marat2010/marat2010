3b419773d5739a7885d6c9133fc2b64d5cca97da
Marat2010 <marat.s.73@mail.ru>
2020-12-05 23:29:48 +0300

    dsd

--- marat2010/settings.py
+++ marat2010/settings.py
@@ -143,17 +143,17 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ('', os.path.join(BASE_DIR, 'portfolio/static')),
-]
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     ('', os.path.join(BASE_DIR, 'portfolio/static')),
+# ]
+#
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# )
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 
