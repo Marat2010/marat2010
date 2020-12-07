ba79ac4b74afbce0cd6790022a44e80d692686d8
Marat2010 <marat.s.73@mail.ru>
2020-12-08 01:24:42 +0300

    as1

--- marat2010/settings.py
+++ marat2010/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ['SECRET_KEY_MARAT2010']
 local_launch = os.environ['local_launch']
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 if local_launch:
     DEBUG = True
